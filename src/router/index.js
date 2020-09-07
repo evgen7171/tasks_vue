d509385128912006df70738e035af942429825e3
evgen7171 <48080044+evgen7171@users.noreply.github.com>
2020-09-07 17:45:31 +0700

    'start_(tasks)'

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+//todo сделать проверку типа provider на запрос данных на backend-e
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
+const routes = [
+    {
+        path: '/',
+        name: 'Create',
+        component: () => import('@/views/Create.vue')
+    },
+    {
+        path: '/list',
+        name: 'list',
+        component: () => import('@/views/List.vue')
+    },
+    {
+        path: '/task/:id',
+        name: 'task',
+        component: () => import('@/views/Task.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
